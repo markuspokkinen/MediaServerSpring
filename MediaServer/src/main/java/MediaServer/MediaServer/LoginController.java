2317a25f6850dce48a87f1fcea95a2bda6ca13bb
Markus Pokkinen <markuspokkinen@gmail.com>
2018-10-04 19:51:12 +0300

    HTML lisätty ja siiretty oikeisiin kansioihin login sivun hakeminen toimii myös post

--- MediaServer/src/main/java/MediaServer/MediaServer/LoginController.java
+++ MediaServer/src/main/java/MediaServer/MediaServer/LoginController.java
@@ -1,14 +1,25 @@
 package MediaServer.MediaServer;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class LoginController {
 
-	@RequestMapping("/Login")
+	@GetMapping("/login")
 	public String login() {
-		return "Hello World";
+		return "login";
 	}
-	
+	@PostMapping("/login")
+	public String logingin(@RequestParam(value="User[Email]")String email,@RequestParam(value="User[Password]")String password) {
+		
+		System.out.println(email);
+		System.out.println(password);
+		// loppu hibernatea
+		
+		return "Profiles";
+	}
 }

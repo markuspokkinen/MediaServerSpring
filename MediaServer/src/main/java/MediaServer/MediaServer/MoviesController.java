f7dc27d53351ddd1dcafd6e525452121b9905214
Markus Pokkinen <markuspokkinen@gmail.com>
2018-10-05 15:56:33 +0300

    ere

--- MediaServer/src/main/java/MediaServer/MediaServer/MoviesController.java
+++ MediaServer/src/main/java/MediaServer/MediaServer/MoviesController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.RedirectView;
 
 import exrternalAPI.API;
 
